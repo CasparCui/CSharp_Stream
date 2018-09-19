b3877d69ee844b894e846d92d132b5d06949746e
Caspar Cui <cui-cekong@live.com>
2018-09-19 17:30:48 +0900

    little change DataDemo =>IEqutable =>+ operator BasicCollect =>Where and Interface

--- GenericAndLinQ/BasicCollect.cs
+++ GenericAndLinQ/BasicCollect.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace Caspar.CSharpTest
 {
     internal class BasicCollect<T> : ICollection<T>
+                            where T : DataDemo, IEquatable<T>
     {
         protected ICollection<T> demo;
 
@@ -17,43 +19,22 @@ namespace Caspar.CSharpTest
             this.demo = demo;
         }
 
-        public int Count => ((ICollection<T>) demo).Count;
+        public int Count => demo.Count;
 
-        public bool IsReadOnly => ((ICollection<T>) demo).IsReadOnly;
+        public bool IsReadOnly => demo.IsReadOnly;
 
-        public void Add(T item)
-        {
-            ((ICollection<T>) demo).Add(item);
-        }
+        public void Add(T item) => demo.Add(item);
 
-        public void Clear()
-        {
-            ((ICollection<T>) demo).Clear();
-        }
+        public void Clear() => demo.Clear();
 
-        public bool Contains(T item)
-        {
-            return ((ICollection<T>) demo).Contains(item);
-        }
+        public bool Contains(T item) => demo.Contains(item);
 
-        public void CopyTo(T[] array, int arrayIndex)
-        {
-            ((ICollection<T>) demo).CopyTo(array, arrayIndex);
-        }
+        public void CopyTo(T[] array, int arrayIndex) => demo.CopyTo(array, arrayIndex);
 
-        public IEnumerator<T> GetEnumerator()
-        {
-            return demo.GetEnumerator();
-        }
+        public IEnumerator<T> GetEnumerator() => demo.GetEnumerator();
 
-        public bool Remove(T item)
-        {
-            return ((ICollection<T>) demo).Remove(item);
-        }
+        public bool Remove(T item) => demo.Remove(item);
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return demo.GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => demo.GetEnumerator();
     }
 }
--- GenericAndLinQ/DataDemo.cs
+++ GenericAndLinQ/DataDemo.cs
@@ -1,9 +1,77 @@
-﻿namespace Caspar.CSharpTest
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Caspar.CSharpTest
+
 {
-    internal class DataDemo
+    internal class DataDemo : IEquatable<DataDemo>
     {
         public string Property1 { get; set; } = "123";
         public string Property2 { get; } = "1234";
         public int? NullInt { get; set; }
+
+        public bool Equals(DataDemo other)
+        {
+            if (this.Property1.Equals(other.Property1) && this.NullInt == other.NullInt)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is DataDemo)
+            {
+                return Equals(obj as DataDemo);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -1445159770;
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Property1);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Property2);
+            hashCode = hashCode * -1521134295 + EqualityComparer<int?>.Default.GetHashCode(NullInt);
+            return hashCode;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public static bool operator ==(DataDemo demo1, DataDemo demo2)
+        {
+            return EqualityComparer<DataDemo>.Default.Equals(demo1, demo2);
+        }
+
+        public static bool operator !=(DataDemo demo1, DataDemo demo2)
+        {
+            return !(demo1 == demo2);
+        }
+
+        public static DataDemo operator +(DataDemo demo1, DataDemo demo2)
+        {
+            DataDemo demoTemp = new DataDemo();
+            var sb = new StringBuilder();
+            sb.Append(demo1.Property1);
+            sb.Append(demo2.Property1);
+            demoTemp.Property1 = sb.ToString();
+            if (demo1.NullInt != null && demo2.NullInt != null)
+            {
+                demoTemp.NullInt = demo1.NullInt + demo2.NullInt;
+            }
+            else
+            {
+                demoTemp.NullInt = demo1.NullInt ?? demo2.NullInt;
+            }
+            return demoTemp;
+        }
     }
 }
